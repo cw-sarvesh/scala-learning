ad294de7295918ed7a40cccc1e36e2aa828e2fba
Sarvesh Mishra <sarvesh.m@carwale.com>
2022-11-28 17:41:48 +0530

    add spark setup

--- build.sbt
+++ build.sbt
@@ -1,8 +1,11 @@
 ThisBuild / version := "0.1.0-SNAPSHOT"
 
-ThisBuild / scalaVersion := "2.13.10"
+ThisBuild / scalaVersion := "2.12.15"
 
 lazy val root = (project in file("."))
   .settings(
     name := "scala-with-spark"
   )
+val sparkVersion = "3.3.1"
+libraryDependencies += "org.apache.spark" %% "spark-core" % sparkVersion % "provided"
+libraryDependencies += "org.apache.spark" %% "spark-sql" % sparkVersion % "provided"
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,30 +1,38 @@
-import greet._
+import org.apache.spark.sql.SparkSession
 
 object Main {
   def main(args: Array[String]): Unit = {
-    println("Hello world!")
-    lazy val geek = {
-      println("Initialization for the first time")
-      12
-    } // Part 1
-    println(geek)
+    val logFile = "./README.md" // Should be some file on your system
+    val spark = SparkSession.builder.appName("Simple Application").config("spark.master", "local").getOrCreate()
+    val logData = spark.read.textFile(logFile).cache()
+    val numAs = logData.filter(line => line.contains("a")).count()
+    val numBs = logData.filter(line => line.contains("b")).count()
+    println(s"Lines with a: $numAs, Lines with b: $numBs")
+    spark.stop()
 
-    // Part 2
-    val addOne = (x: Int) => x + 1
-    println(geek)
-    println({
-      val x = 1 + 1
-      x + 1
-    }) // 3
-    println(addThenMultiply(2, 3)(2))
-    println("Hello, " + name + "!")
-    // Importing from self package main
-    val greeter = new Greeter("Hello, ", "!")
-    // Importing from other package i.e. greet
-    val greeter2 = new hello("Hello, ", "!")
-    greeter.greet("Scala developer")
-    greeter2.greeter("Scala developer from other package")
-    println(greeter2.sqrtplus1(4))
+    //    println("Hello world!")
+    //    lazy val geek = {
+    //      println("Initialization for the first time")
+    //      12
+    //    } // Part 1
+    //    println(geek)
+    //
+    //    // Part 2
+    //    val addOne = (x: Int) => x + 1
+    //    println(geek)
+    //    println({
+    //      val x = 1 + 1
+    //      x + 1
+    //    }) // 3
+    //    println(addThenMultiply(2, 3)(2))
+    //    println("Hello, " + name + "!")
+    //    // Importing from self package main
+    //    val greeter = new Greeter("Hello, ", "!")
+    //    // Importing from other package i.e. greet
+    //    val greeter2 = new hello("Hello, ", "!")
+    //    greeter.greet("Scala developer")
+    //    greeter2.greeter("Scala developer from other package")
+    //    println(greeter2.sqrtplus1(4))
   }
 
   def addThenMultiply(x: Int, y: Int)(multiplier: Int): Int = {
