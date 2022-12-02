3389b612395d6f27dd685d17731e706046891706
Sarvesh Mishra <sarvesh.m@carwale.com>
2022-12-02 16:15:27 +0530

    update files

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,13 +2,20 @@ import org.apache.spark.sql.SparkSession
 
 object Main {
   def main(args: Array[String]): Unit = {
-    val carCsv = "./src/Resources/Car_data.csv" // Should be some file on your system
+    //    val carCsv = "./src/Resources/synapse.csv" // Should be some file on your system
+    val table1 = "./src/Resources/table1.csv"
+    val table2 = "./src/Resources/table2.csv"
     val spark = SparkSession.builder.appName("Simple Application")
-      .config("spark.master", "local")
+      .master("local[1]")
+      .appName("SparkByExamples")
       .getOrCreate()
-    val CarDf = spark.read.option("header", "true").option("inferSchema", "true").csv(carCsv)
-    println(CarDf.printSchema)
-    println(CarDf.show)
+    val Table1Df = spark.read.option("header", "true").option("inferSchema", "true").csv(table1)
+    val Table2Df = spark.read.option("header", "true").option("inferSchema", "true").csv(table2)
+    Table1Df.createOrReplaceTempView("table1Df")
+    Table2Df.createOrReplaceTempView("table2Df")
+    spark.sql("select table1Df.first_name, table2Df.first_name from table1Df, table2Df where table1Df.city = table2Df.city").show
+    //    println(Table1Df.printSchema)
+    //    println(Table1Df.show)
     spark.stop()
   }
 }
